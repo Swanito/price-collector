b581bdf720017dc8eb8b03b37b3fc8d45d57c43a
Swanito <jon.gonzlez@gmail.com>
2022-01-25 21:07:51 +0100

    added initial index to scrapers

--- cypress/integration/scrap-nes.js
+++ cypress/integration/scrap-nes.js
@@ -1,11 +1,13 @@
 import Search from '../support/ebay-script';
 
+const initial = Cypress.env("INITIAL_INDEX") || 0;
+
 describe('ebay', () => {
     Cypress.on('uncaught:exception', (err, runnable) => {
         return false;
     });
 
     it('Searches for NES', () => {
-        Search('NES', './input-files/nes.json');
+        Search('NES', './input-files/nes.json', initial);
     });
 });
--- cypress/integration/scrap-snes.js
+++ cypress/integration/scrap-snes.js
@@ -1,11 +1,13 @@
 import Search from '../support/ebay-script';
 
+const initial = Cypress.env("INITIAL_INDEX") || 0;
+
 describe('ebay', () => {
     Cypress.on('uncaught:exception', (err, runnable) => {
         return false;
     });
 
     it('Searches for SNES', () => {
-        Search('SNES', './input-files/snes.json');
+        Search('SNES', './input-files/snes.json', initial);
     });
 });
