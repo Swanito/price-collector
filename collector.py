c1077f52a1e54515c9785b68c12fdf6b51629e50
Jon Gonzalez Lavin <jon.gonzalez.lavin@accenture.com>
2022-06-29 09:15:15 +0200

    headless

--- collector.py
+++ collector.py
@@ -22,6 +22,7 @@ def collect(path):
 
     driver_path = './chromedriver_linux'
     options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-gpu')
     options.add_argument('--disable-dev-shm-usage')
